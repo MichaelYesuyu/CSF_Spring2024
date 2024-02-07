78c6a68e157ee61844615d63e6039fcc8bf66792
Suyu Ye <sye10@jh.edu>
2024-02-06 19:11:39 -0500

    Added helper function declarations to .h file

--- hw2/csf_assign02/drawing_funcs.h
+++ hw2/csf_assign02/drawing_funcs.h
@@ -28,7 +28,30 @@ void draw_sprite(struct Image *img,
                  struct Image *spritemap,
                  const struct Rect *sprite);
 
-//Helper function to calculate the color value of the new pixel
-uint32_t calculate_color(uint32_t background_color, uint32_t foreground_color);
+int32_t in_bounds(struct Image *img, int32_t x, int32_t y); 
+uint32_t compute_index(struct Image *img, int32_t x, int32_t y);
+int32_t clamp(int32_t val, int32_t min, int32_t max);
+
+//Get R component of RGB
+uint8_t get_r(uint32_t color);
+
+//Get G component of RGB
+uint8_t get_g(uint32_t color);
+
+//Get B component of RGB
+uint8_t get_b(uint32_t color);
+
+//Get A component of RGB
+uint8_t get_a(uint32_t color);
+
+//Calculates the value of blended individual components (RGB)
+uint8_t blend_components(uint8_t fg, uint8_t bg, uint8_t alpha);
+
+//Helper function to blend the colors
+uint32_t blend_colors(uint32_t fg, uint32_t bg);
+
+void set_pixel(struct Image *img, uint32_t index, uint32_t color);
+int64_t square(int64_t x);
+int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
 
 #endif // DRAWING_FUNCS_H
