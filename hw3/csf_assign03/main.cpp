a143d0b10a059e75aab6a27046d46a077a0e2814
Suyu Ye <sye10@jh.edu>
2024-03-09 14:07:56 -0500

    Added a bit of reading

--- hw3/csf_assign03/main.cpp
+++ hw3/csf_assign03/main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include <string>
 #include "functions.h"
 
-int main() {
+using std::ifstream;
+using std::stoi;
+
+//Sample command line input
+// ./csim 256 4 16 write-allocate write-back lru < sometracefile
+
+int main(int argc, char** argv) {
+    int numSets = stoi(argv[1]);
+    int numBlocks = stoi(argv[2]);
+    int bytesOfMemory = stoi(argv[3]);
     return 0;
 }
