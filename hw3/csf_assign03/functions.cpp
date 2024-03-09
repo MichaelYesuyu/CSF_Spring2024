47abdef97c2352a37089f0d13791d58aacad7db8
Ruoxi Liu <rliu79@ugradx.cs.jhu.edu>
2024-03-09 15:56:55 -0500

    function

--- hw3/csf_assign03/functions.cpp
+++ hw3/csf_assign03/functions.cpp
@@ -1,9 +1,14 @@
 #include "functions.h"
 
-Cache cache create_cache(int numSets, int numBlocks, int bytesOfMemory){
-     for (int i = 0; i < numSets; i++){
+Cache create_cache(int numSets, int numBlocks, int bytesOfMemory){
+    Cache cache;
+    for (int i = 0; i < numSets; i++){
+        Set set;
         for (int j = 0; j < numBlocks; j++){
-            
+            Slot slot = new Slot();
+            set.slots.push_back(slot);
         }
+        cache.sets.push_back(set);
     }
+    return cache;
 }
--- hw3/csf_assign03/functions.h
+++ hw3/csf_assign03/functions.h
@@ -37,6 +37,7 @@ struct Cache {
     std::vector<Set> sets;
 };
 
+Cache create_cache(int numSets, int numBlocks, int bytesOfMemory);
 
 
 
