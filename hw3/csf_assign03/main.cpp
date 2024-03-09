dcfb105dffbeae9f493e4d32193444772aefb8cf
Suyu Ye <sye10@jh.edu>
2024-03-09 17:47:57 -0500

    Wrote load miss LRU

--- hw3/csf_assign03/main.cpp
+++ hw3/csf_assign03/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <pair>
+#include <tuple>
 #include "functions.h"
 
 using std::ifstream;
@@ -14,6 +14,8 @@ using std::ifstream;
 using std::stringstream;
 using std::cerr;
 using std::cin;
+using std::tuple;
+using std::get;
 
 //Sample command line input
 // ./csim 256 4 16 write-allocate write-back lru < sometracefile
@@ -43,11 +45,27 @@ int main(int argc, char** argv) {
         string command;
         string address;
         ss >> command >> address;
+
+
         //Increment simulation timestep
         simulation_timestep++;
-        cout << command << " " << address << endl;
-        //Call some function to handle
-        //updateCache(command, address, type_write_miss, type_write_hit, replace_strategy);
+        //Look for the element inside the cache
+        tuple<int32_t, int32_t> result = find(cache, get_index(stoi(address), numSets, bytesOfMemory), get_tag(stoi(address), numSets, bytesOfMemory));
+        //Loading
+        if(get<0>(result) == -1){ //Element was not found
+            if(replace_strategy == "lru"){
+                handle_load_miss_LRU();
+            } else if (replace_strategy == "fifo") {
+                handle_load_miss_FIFO();
+            } else {
+                cerr << "Invalid load strategy" << endl;
+                return 1;
+            }
+        } else { //Element was found
+            handle_load_hit(cache, get<0>(result), get<1>(result), numBlocks);
+        }
+        
+        
     }
 
     //Testing
@@ -62,21 +80,30 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-Slot find(Cache& cache, uint32_t index, uint32_t tag){
-
-}
-
-void handle_cache_hit(Cache& cache, uint32_t indexSet, uint32_t indexSlot, uint32_t numBlocks){
+void handle_load_hit(Cache& cache, uint32_t indexSet, uint32_t indexSlot, uint32_t numBlocks){
     for(int i = 0; i < numBlocks; i++){
         cache.sets[indexSet].slots[i].access_ts++;
     }
     cache.sets[indexSet].slots[indexSlot].access_ts = 0;
 }
 
-void handle_cache_miss_LRU(){
-    
+void handle_load_miss_LRU(Cache& cache, uint32_t indexSet, uint32_t numBlocks, Slot newSlot){
+    //Increment all the access_ts to ensure all the access_ts are at least 1
+    for(int i = 0; i < numBlocks; i++){
+        cache.sets[indexSet].slots[i].access_ts++;
+    }
+    int max = 0;
+    int index_LRU = 0;
+    //Look for the least recently used slot and replace it with the new slot
+    for(int i = 0; i < numBlocks; i++){
+        if(cache.sets[indexSet].slots[i].access_ts > max){
+            max = cache.sets[indexSet].slots[i].access_ts;
+            index_LRU = i;
+        }
+    }
+    cache.sets[indexSet].slots[index_LRU] = newSlot;
 }
 
-void handle_cache_miss_FIFO(){
+void handle_load_miss_FIFO(){
 
 }
