14ec9fea4c32243d8662dd8e2516cad5fca3d33a
Suyu Ye <sye10@jh.edu>
2024-03-09 14:11:14 -0500

    minor updates

--- hw3/csf_assign03/main.cpp
+++ hw3/csf_assign03/main.cpp
@@ -4,6 +4,7 @@
 
 using std::ifstream;
 using std::stoi;
+using std::string;
 
 //Sample command line input
 // ./csim 256 4 16 write-allocate write-back lru < sometracefile
@@ -12,5 +13,7 @@ int main(int argc, char** argv) {
     int numSets = stoi(argv[1]);
     int numBlocks = stoi(argv[2]);
     int bytesOfMemory = stoi(argv[3]);
+    string type_write_miss = argv[4];
+    string type_write_hit = argv[5];
     return 0;
 }
