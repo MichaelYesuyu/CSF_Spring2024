02de8981954921a7f9896809840d5dd86456fcf0
Suyu Ye <sye10@jh.edu>
2024-03-09 15:43:48 -0500

    Added more to main

--- hw3/csf_assign03/main.cpp
+++ hw3/csf_assign03/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include "cacheSimulator.h"
 
 using std::ifstream;
@@ -7,28 +9,44 @@ using std::stoi;
 using std::string;
 using std::cout;
 using std::endl;
+using std::ifstream;
+using std::stringstream;
+using std::cerr;
+using std::cin;
 
 //Sample command line input
 // ./csim 256 4 16 write-allocate write-back lru < sometracefile
 
 int main(int argc, char** argv) {
+    //Handle case of invalid number of parameters
+    if(argc != 7){
+        cerr << "Invalid parameters" << endl;
+        return 1;
+    }
+    //Read the command line arguments
     int numSets = stoi(argv[1]);
     int numBlocks = stoi(argv[2]);
     int bytesOfMemory = stoi(argv[3]);
     string type_write_miss = argv[4];
     string type_write_hit = argv[5];
     string replace_strategy = argv[6];
-    string fileName = argv[8];
 
-
-
-
+    //Read the trace file and update cache
+    string line;
+    while(getline(cin, line)){
+        stringstream ss(line);
+        string command;
+        string address;
+        ss >> command >> address;
+        cout << command << " " << address << endl;
+        //Call some function to handle
+        //updateCache(command, address, type_write_miss, type_write_hit, replace_strategy);
+    }
 
     //Testing
     cout << "numSets: " << numSets << endl << "numBlocks: " << numBlocks  << endl << "bytesOfMemory " << bytesOfMemory << endl;
     cout << type_write_miss << endl;
     cout << type_write_hit << endl;
     cout << replace_strategy << endl;
-    cout << fileName << endl;
     return 0;
 }
