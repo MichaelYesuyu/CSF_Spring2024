4852b5d14af2500b1169c0895c65272c5b56e203
Ruoxi Liu <rliu79@ugradx.cs.jhu.edu>
2024-03-09 19:50:38 -0500

    added headers

--- hw3/csf_assign03/functions.h
+++ hw3/csf_assign03/functions.h
@@ -41,7 +41,12 @@ Cache create_cache(int numSets, int numBlocks);
 
 uint32_t get_tag(uint32_t address, uint32_t numSets, uint32_t bytesOfMemory);
 uint32_t get_index(uint32_t address, uint32_t numSets, uint32_t bytesOfMemory);
+std::tuple<int32_t, int32_t> find(Cache cache, uint32_t index, uint32_t in_tag);
 
+void load(uint32_t address, Cache cache, uint32_t numSets, uint32_t numBlocks, uint32_t bytesOfMemory, string replace_strategy);
+void handle_load_hit(Cache& cache, uint32_t indexSet, uint32_t indexSlot, uint32_t simulation_timestep);
+void handle_load_miss_LRU(Cache& cache, uint32_t indexSet, uint32_t numBlocks, Slot newSlot);
+void handle_load_miss_FIFO(Cache& cache, uint32_t indexSet, uint32_t numBlocks, Slot newSlot);
 
 
 
