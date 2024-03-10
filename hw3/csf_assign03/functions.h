4bed8caebdbae3dd6ba8a1f53e21ef87297f3ee5
Ruoxi Liu <rliu79@ugradx.cs.jhu.edu>
2024-03-09 19:56:38 -0500

    .h

--- hw3/csf_assign03/functions.h
+++ hw3/csf_assign03/functions.h
@@ -35,18 +35,24 @@ struct Set {
 
 struct Cache {
     std::vector<Set> sets;
+    uint32_t numSets;
+    uint32_t numBlocks;
+    uint32_t bytesOfMemory;
+    string replace_strategy;
+    string type_write_miss;
+    string type_write_hit;
 };
 
-Cache create_cache(int numSets, int numBlocks);
+Cache create_cache(uint32_t numSets, uint32_t numBlocks, uint32_t bytesOfMemory, string replace_strategy, string type_write_miss, string type_write_hit);
 
 uint32_t get_tag(uint32_t address, uint32_t numSets, uint32_t bytesOfMemory);
 uint32_t get_index(uint32_t address, uint32_t numSets, uint32_t bytesOfMemory);
 std::tuple<int32_t, int32_t> find(Cache cache, uint32_t index, uint32_t in_tag);
 
-void load(uint32_t address, Cache cache, uint32_t numSets, uint32_t numBlocks, uint32_t bytesOfMemory, string replace_strategy);
-void handle_load_hit(Cache& cache, uint32_t indexSet, uint32_t indexSlot, uint32_t simulation_timestep);
-void handle_load_miss_LRU(Cache& cache, uint32_t indexSet, uint32_t numBlocks, Slot newSlot);
-void handle_load_miss_FIFO(Cache& cache, uint32_t indexSet, uint32_t numBlocks, Slot newSlot);
+void load(uint32_t address, Cache cache);
+void handle_load_hit(Cache& cache, uint32_t simulation_timestep);
+void handle_load_miss_LRU(Cache& cache, Slot newSlot);
+void handle_load_miss_FIFO(Cache& cache, Slot newSlot);
 
 
 
